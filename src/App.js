a0cf36c49eb148ad323deaa90f2ca1193e4cd71f
Sohail Asghar <sohailasghar.workofficial@gmail.com>
2024-03-31 19:29:18 +0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TicTacToe from './components/tic-tac-toe';
+
+function App() {
+  return(
+    <div className="App">
+        {/* Tic-Tac-Toe */}
+        <TicTacToe/>
+
+    </div>
+);
+}
+
+export default App;
